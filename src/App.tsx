7c19e5ae28d524d60781758654445a886fbaf9c2
mrprince88 <mrprince0088@gmail.com>
2024-01-05 11:40:15 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import CallsContainer from "./components/CallsContainer";
+
+const App = () => {
+  return (
+    <div className="appContainer">
+      <Header />
+      <CallsContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CallsContainer/index.tsx
@@ -0,0 +1,9 @@
+import classes from "./styles.module.css";
+
+export default function CallsContainer() {
+  return (
+    <div className={classes.container}>
+      <h1>CallsContainer</h1>
+    </div>
+  );
+}
